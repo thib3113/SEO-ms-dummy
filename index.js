640dd677f835a8b6948390f2ee522a8513dd4c53
thibaut severac <thib3113@gmail.com>
2018-04-21 12:12:46 +0200

    add some logs

--- index.js
+++ index.js
@@ -4,30 +4,38 @@ import mongoose  from "mongoose";
 import {console} from "./config";
 
 async.parallel({
-                   mongo : function (callback) {
+                  mongo : function (callback) {
 
                        //mongodb://utilisateur:motdepasse@url:port
                        //attention, utilisateur et motdepasse doivent être url compliant
-
+                       console.debug("start mongo connection");
                        mongoose.connect("mongodb://root:ynov2018@nexus2.devandstudy.com:9020")
                                .then(() => {
+                                   console.debug("mongo connected");
                                    callback(null, true);
                                })
                                .catch(err => {
+                                   console.debug("mongo connection failed", err);
                                    callback(err);
                                });
-
                    },
                    rabbit: function (callback) {
                        //connect to rabbitMQ
 
                        //amqp://utilisateur:motdepasse@url:port
                        //attention, utilisateur et motdepasse doivent être url compliant
-
-                       // amqp://rabbit:9672
                        amqp.connect("amqp://ynov:ynov2018@nexus2.devandstudy.com:9672", function (err, conn) {
-                           if (err) return callback(err);
+                           if (err) {
+                               console.debug("rabbitMQ connection failed");
+                               return callback(err);
+                           }
                            conn.createChannel(function (err, ch) {
+                               if (!err) {
+                                   console.debug("rabbitMQ connected");
+                               }
+                               else {
+                                   console.debug("rabbitMQ connection failed");
+                               }
                                callback(err, ch);
                            });
                        });
